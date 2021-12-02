3995695c1c34723f39649d64bd4fc2e212568423
Ashymaliev <Nursultan-99kg@mail.ru>
2021-12-02 21:18:33 +0600

    selector

---
+++ script.js
@@ -0,0 +1,7 @@
+const progress = document.getElementById("progress")
+const prev = document.getElementById("prev")
+const mext = document.getElementById("next")
+
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
